924bf7df203e4b3513d05da4e5a6d79bd2c2c444
Riyaz Hawaldar <riyazh@cybage.com>
2018-12-21 17:53:54 +0530

    app created

---
+++ routes/product.route.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const product_controller = require('../controllers/product.controller');
+
+
+//localhost:3000/products/create
+router.post('/create', product_controller.product_create);
+
+
+//localhost:3000/products/PRODUCT_ID
+router.get('/:id', product_controller.product_details);
+
+
+//localhost:3000/products/PRODUCT_ID/update
+router.put('/:id/update', product_controller.product_update);
+
+
+//localhost:3000/products/PRODUCT_ID/delete
+router.delete('/:id/delete', product_controller.product_delete);
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', product_controller.test);
+module.exports = router;
+
